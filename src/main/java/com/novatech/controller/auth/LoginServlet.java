1e1fc9c1c8ab8ebaf9235d8887a183965dd5db4a
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-05-14 12:49:21 +0000

    create loginservlet controller

---
+++ src/main/java/com/novatech/controller/auth/LoginServlet.java
@@ -0,0 +1,47 @@
+package com.novatech.controller.auth;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.novatech.dao.UserDAO;
+import com.novatech.model.User;
+
+public class LoginServlet extends HttpServlet {
+    private UserDAO userDAO;
+
+    @Override
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("views/auth/login.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = userDAO.validateUser(username, password);
+
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("userId", user.getUserId());
+            session.setAttribute("username", user.getUsername());
+            response.sendRedirect("tasks");
+        } else {
+            request.setAttribute("error", "Invalid credentials");
+            request.getRequestDispatcher("views/auth/login.jsp").forward(request, response);
+        }
+    }
+    
+}
