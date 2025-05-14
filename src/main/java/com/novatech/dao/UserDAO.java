d5b9472e375366889592919fc42a3d83f7071837
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-05-14 11:45:17 +0000

    add user data access object

---
+++ src/main/java/com/novatech/dao/UserDAO.java
@@ -0,0 +1,151 @@
+package com.novatech.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.novatech.model.User;
+import com.novatech.util.DatabaseConfig;
+
+public class UserDAO {
+    public boolean addUser(User user) {
+        String query = "INSERT INTO User (username, password, email, created_at) VALUES (?,?,?,?)";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
+            statement.setTimestamp(4,user.getCreatedAt());
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (Exception e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User getUserByUsername(String username) {
+        String query = "SELECT * FROM User WHERE username = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, username);
+
+            java.sql.ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setUserId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setEmail(resultSet.getString("email"));
+                user.setCreatedAt(resultSet.getTimestamp("created_at"));
+                return user;
+            }
+            return null;
+        } catch (Exception e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User getUserByEmail(String email){
+        String query = "SELECT * FROM User WHERE email = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+
+            java.sql.ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setUserId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setEmail(resultSet.getString("email"));
+                user.setCreatedAt(resultSet.getTimestamp("created_at"));
+                return user;
+            }
+            return null;
+        } catch (Exception e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User getUserById(int userId){
+        String query = "SELECT * FROM User WHERE id = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, userId);
+
+            java.sql.ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setUserId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                user.setEmail(resultSet.getString("email"));
+                user.setCreatedAt(resultSet.getTimestamp("created_at"));
+                return user;
+            }
+            return null;
+        } catch (Exception e) {
+            System.err.println("Error creating user: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean updateUser(User user) {
+        String query = "UPDATE User SET username = ?, email = ?, WHERE id = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setInt(3, user.getUserId());
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            System.err.println("Error updating user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean updateUserPassword(int userId, String newPassword) {
+        String query = "UPDATE User SET password = ? WHERE id = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, newPassword);
+            statement.setInt(2, userId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            System.err.println("Error updating user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteUser(int userId) {
+        String query = "DELETE FROM User WHERE id = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, userId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            System.err.println("Error deleting user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+}
