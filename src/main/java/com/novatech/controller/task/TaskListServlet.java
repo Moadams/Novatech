aa3cf8cc0425df2c5b5165370141cc4511355e7b
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-05-15 21:20:05 +0000

    render tasks lists

--- src/main/java/com/novatech/controller/task/TaskListServlet.java
+++ src/main/java/com/novatech/controller/task/TaskListServlet.java
@@ -14,8 +14,14 @@ public class TaskListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
         int userId = (int) request.getSession().getAttribute("userId");
+        if(session == null || session.getAttribute("userId") == null){
+            response.sendRedirect("login");
+            return;
+        }
         List<Task> tasks = taskDao.getTasksByUserId(userId);
+        
         request.setAttribute("tasks", tasks);
         RequestDispatcher dispatcher = request.getRequestDispatcher("views/tasks/list.jsp");
         dispatcher.forward(request, response);
